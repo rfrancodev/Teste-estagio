fcfeab08042666fce50215a1657e6be866dc626b
rfrancodev <francorfs@icloud.com>
2024-09-29 09:56:04 -0300

    Sequencia realizada

--- Fibonacci/index.js
+++ Fibonacci/index.js
@@ -1,2 +1,23 @@
 
-function Fibonacci(numero)
+function fibonacci(num) {
+
+let n1 = 0, n2 = 1, novoNumero;
+
+    if (num === 0 || num === 1) {
+        return `O numero ${num} pertence a sequencia`
+    }
+
+    novoNumero = n1 + n2
+        while (novoNumero <= num) {
+            if (novoNumero === num) {
+                return `O numero ${num} pertence a sequencia`
+            }
+            n1=n2
+            n2=novoNumero
+            novoNumero = n1+n2
+        }
+        return `O numero ${num}  não pertence a sequencia`
+}
+
+const numero = 6
+console.log(fibonacci(numero));
